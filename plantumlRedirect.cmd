05bcc86e64501b49ea5026aae4ef95b390155c9a
Steven van Dijk <zteven@gmail.com>
2024-07-05 10:51:45 +0000

    Make working with cml file easier

--- plantumlRedirect.cmd
+++ plantumlRedirect.cmd
@@ -1,1 +1,8 @@
-plantuml -pipe < $1 > $2
+#!/bin/bash
+
+for file in src-gen/$1_*Context.puml; do
+    if [ -e "$file" ]; then
+        plantuml -tpng -pipe < "$file" > tmp.png
+        cp tmp.png "$(basename "$file" .puml).png"
+    fi
+done
